f3942cb734233c8a1af64b9099c7a93ca1b11b85
Nick Ragaz <nick.ragaz@gmail.com>
2012-07-17 15:16:16 -0400

    raise error if record not persisted

--- lib/resque/plugins/async/method.rb
+++ lib/resque/plugins/async/method.rb
@@ -3,19 +3,23 @@ require 'resque/plugins/async/worker'
 module Resque::Plugins::Async::Method
   extend ActiveSupport::Concern
 
+  class NotPersistedError < StandardError; end
+
   module ClassMethods
     def async_method(method_name, opts={})
       # Allow tests to call sync_ methods ...
       alias_method :"sync_#{method_name}", method_name
-    
+
       # ... but don't actually make them asynchronous
       return if Rails.env.test?
-    
+
       define_method "#{method_name}" do |*args|
+        raise NotPersistedError, "Methods can only be async'ed on persisted records (currently: #{inspect})" unless persisted?
+
         my_klass       = Resque::Plugins::Async::Worker
         my_klass.queue = opts[:queue] ||
                          send(:class).name.underscore.pluralize
-    
+
         Resque.enqueue(
           my_klass,
           send(:class).name,
--- test/async_method_test.rb
+++ test/async_method_test.rb
@@ -16,6 +16,14 @@ class AsyncMethodTest < ActiveSupport::TestCase
     assert_equal 'success!', user.sync_long_method
   end
 
+  test "raise error if not persisted" do
+    user = User.new
+
+    assert_raises(Resque::Plugins::Async::Method::NotPersistedError) do
+      user.long_method
+    end
+  end
+
   test "lint" do
     assert_nothing_raised do
       Resque::Plugin.lint(Resque::Plugins::Async::Method)
